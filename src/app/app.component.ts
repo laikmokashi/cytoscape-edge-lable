58df21389368d4de91d545aa2fc7d41beef95994
laikmokashi <laikmokashi20@gmail.com>
2024-05-30 00:23:23 +0530

    new version of extension is added 1.0.4
    we are now able to add html on node also

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,20 +40,9 @@ export class AppComponent implements OnInit {
 
           position: {
             // the model position of the node (optional on init, mandatory after)
-            x: 100,
-            y: 100,
+            x: 0,
+            y: 0,
           },
-
-          selected: false, // whether the element is selected (default false)
-
-          selectable: true, // whether the selection state is mutable (default true)
-
-          locked: false, // when locked a node's position is immutable (default false)
-
-          grabbable: true, // whether the node can be grabbed and moved by the user
-
-          pannable: false, // whether dragging the node causes panning instead of grabbing
-
           classes: ['foo', 'bar'], // an array (or a space separated string) of class names that the element has
 
           // DO NOT USE THE `style` FIELD UNLESS ABSOLUTELY NECESSARY
@@ -67,13 +56,19 @@ export class AppComponent implements OnInit {
         {
           // node n2
           data: { id: 'n2' },
-          renderedPosition: { x: 200, y: 200 }, // can alternatively specify position in rendered on-screen pixels
+          renderedPosition: { x: 400, y: 400 }, // can alternatively specify position in rendered on-screen pixels
         },
 
         {
           // node n3
           data: { id: 'n3', parent: 'nparent' },
-          position: { x: 123, y: 234 },
+          position: { x: -123, y: 134 },
+        },
+
+        {
+          // node n2
+          data: { id: 'n4' },
+          renderedPosition: { x: 600, y: 600 }, // can alternatively specify position in rendered on-screen pixels
         },
 
         {
@@ -93,6 +88,16 @@ export class AppComponent implements OnInit {
 
           pannable: true, // whether dragging on the edge causes panning
         },
+        {
+          // edge e2
+          data: {
+            id: 'e2',
+            // inferred as an edge because `source` and `target` are specified:
+            source: 'n2', // the source node id (edge comes from this node)
+            target: 'n4', // the target node id (edge goes to this node)
+            // (`source` and `target` can be effectively changed by `eles.move()`)
+          },
+        },
       ],
 
       layout: {
@@ -111,12 +116,20 @@ export class AppComponent implements OnInit {
           selector: 'edge',
           style: {},
         },
+        {
+          selector: '#e2',
+          style: {
+            'curve-style': 'unbundled-bezier',
+            'control-point-weights': '0.25 0.5 0.75',
+            'control-point-distances': '-50 0 50',
+          },
+        },
       ],
     });
 
     this.cy.nodeHtmlLabel([
       {
-        query: 'edge', // cytoscape query selector
+        query: '#e1', // cytoscape query selector
         halign: 'center', // title vertical position. Can be 'left',''center, 'right'
         valign: 'center', // title vertical position. Can be 'top',''center, 'bottom'
         halignBox: 'center', // title vertical position. Can be 'left',''center, 'right'
@@ -126,9 +139,45 @@ export class AppComponent implements OnInit {
         edgehtmlTiltPoint1: 'sourceNode', // first point to get angle of tilt for html.Can be 'sourceNode','targetNode', control point (i.e 0,1,2...)
         edgehtmlTiltPoint2: 'targetNode', // second point to get angle of tilt for html.Can be 'sourceNode','targetNode', control point (i.e 0,1,2...)
         tpl(data: any) {
-          return `<div style='background:greenyellow;color:'red'> this is a sample html <button onclick="console.log(
+          return `<div style='background:greenyellow;color:red;width:100px'> click and check console <button onclick="console.log(
             'button clicked'
           )">click</button></div>`;
+          // your html template here
+        },
+      },
+      {
+        query: '#e2', // cytoscape query selector
+        halign: 'center', // title vertical position. Can be 'left',''center, 'right'
+        valign: 'center', // title vertical position. Can be 'top',''center, 'bottom'
+        halignBox: 'center', // title vertical position. Can be 'left',''center, 'right'
+        valignBox: 'center', // title relative box vertical position. Can be 'top',''center, 'bottom'
+        cssClass: '', // any classes will be as attribute of <div> container for every title,
+        edgehtmlLocation: 'center', //location on edge to render html. Can be 'start','end','center'. 'center' will be default
+        edgehtmlTiltPoint1: 0, // first point to get angle of tilt for html.Can be 'sourceNode','targetNode', control point (i.e 0,1,2...)
+        edgehtmlTiltPoint2: 2, // second point to get angle of tilt for html.Can be 'sourceNode','targetNode', control point (i.e 0,1,2...)
+        tpl(data: any) {
+          return `<div > 
+          <div style='color:yellow ;background:black; margin:5px'>Above Edge</div> 
+          <div style='color:red ;background:black; margin:5px'>below Edge</div> 
+          </div>`;
+          // your html template here
+        },
+      },
+      {
+        query: 'node', // cytoscape query selector
+        halign: 'center', // title vertical position. Can be 'left',''center, 'right'
+        valign: 'center', // title vertical position. Can be 'top',''center, 'bottom'
+        halignBox: 'center', // title vertical position. Can be 'left',''center, 'right'
+        valignBox: 'center', // title relative box vertical position. Can be 'top',''center, 'bottom'
+        cssClass: '', // any classes will be as attribute of <div> container for every title,
+        edgehtmlLocation: 'center', //location on edge to render html. Can be 'start','end','center'. 'center' will be default
+        edgehtmlTiltPoint1: 0, // first point to get angle of tilt for html.Can be 'sourceNode','targetNode', control point (i.e 0,1,2...)
+        edgehtmlTiltPoint2: 2, // second point to get angle of tilt for html.Can be 'sourceNode','targetNode', control point (i.e 0,1,2...)
+        tpl(data: any) {
+          return `<div style='background:yellow'> 
+          <em>this is </em>
+          <div><strong>Node </strong><div>  
+          </div>`;
           // your html template here
         },
       },
