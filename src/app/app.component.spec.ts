0fb440395d0af4083d8fdf0f0a99b8b5aa92e270
laikmokashi <laikmokashi20@gmail.com>
2024-05-31 00:38:41 +0530

    change name and build again

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'edge-lable'`, () => {
+  it(`should have as title 'cytoscape-node-edge-html-lable-example'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('edge-lable');
+    expect(app.title).toEqual('cytoscape-node-edge-html-lable-example');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('edge-lable app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'cytoscape-node-edge-html-lable-example app is running!'
+    );
   });
 });
